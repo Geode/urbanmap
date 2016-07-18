46c64ee643a0e3e11f744c9cade14ebd32992fa5
Nicolas Dufrane <nicolas.dufrane@opengeode.be>
2016-07-18 13:28:44 +0200

    consume civadis map parameters

---
+++ urbanmap/public/app/lib/App/civadis-map.js
@@ -0,0 +1,45 @@
+
+/*
+ * @include App/main-layout.js
+ */
+
+Ext.namespace("UrbanMap");
+
+(function() {
+
+    // global settings
+
+    // Override print url for opengeode production
+    //printCapabilities.printURL = "http://urbanmap.opengeode.be/print/print.pdf?id=None";
+    //printCapabilities.createURL = "http://urbanmap.opengeode.be/print/create.json?id=None";
+
+    OpenLayers.ImgPath = "lib/openlayers/img/";
+    OpenLayers.ProxyHost = UrbanMap.config.proxy_url;
+
+    //ExtJS QuickTips enable
+    Ext.QuickTips.init();
+
+    // run App.layout.init() when the page
+    // is ready
+    Ext.onReady(function() {
+    	var pageParameters = Ext.urlDecode(window.location.search.substring(1));
+        var parcelles = pageParameters.idParcelle instanceof Array ? pageParameters.idParcelle : [pageParameters.idParcelle];
+        var licences  = pageParameters.idPermis instanceof Array ? pageParameters.idPermis : [pageParameters.idPermis];
+        var ins = pageParameters.ins;
+
+        NISNum = ins;
+        urbanCapakeyArray = parcelles;
+        //TODO Do something with licences
+
+    	UrbanMap.layout.init();
+
+        var hideMask = function () {
+            Ext.get('loading').remove();
+            Ext.fly('loading-mask').fadeOut({
+                remove:true
+            });
+        };
+
+        hideMask.defer(250);
+    });
+})();
